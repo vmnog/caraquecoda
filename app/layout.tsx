fd75dea85af5ef9725f7827f60c020a19afd5bb4
vmnog <vmnogueira.ti@gmail.com>
2024-05-28 18:04:44 -0300

    chore: disable email button while input is empty

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
-          <Header />
+          {/* <Header /> */}
           {children}
           {/* <Banner /> */}
         </div>
--- components/join-mentorship.tsx
+++ components/join-mentorship.tsx
@@ -52,14 +52,14 @@ export default function JoinMentorship() {
             {/* CTA content */}
             <div className="mb-6 lg:mr-16 lg:mb-0 text-center lg:text-left lg:w-1/2">
               <h3 className="h3 text-white mb-2">Quer acelerar seus estudos?</h3>
-              <p className="text-purple-200 text-lg">Faça parte nossa mentoria para conquistar seu emprego com programação antes de qualquer outra pessoa.</p>
+              <p className="text-purple-200 text-lg">Faça parte da nossa mentoria para conquistar seu emprego com programação antes de qualquer outra pessoa.</p>
             </div>
 
             {/* CTA form */}
             <form className="w-full lg:w-1/2" onSubmit={handleSubmitEmail}>
               <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:max-w-none">
-                <input type="email" value={email} onChange={e => setEmail(e.target.value)} disabled={wasEmailSent} className="w-full appearance-none bg-purple-700 border border-purple-500 focus:border-purple-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-400 disabled:opacity-30" placeholder="Seu melhor e-mail…" aria-label="Seu melhor e-mail…" />
-                <button disabled={wasEmailSent} type="submit" className="btn text-purple-600 bg-purple-100 hover:bg-white shadow disabled:opacity-30">Assinar</button>
+                <input id="email" name="email" type="email" value={email} onChange={e => setEmail(e.target.value)} disabled={wasEmailSent} className="w-full appearance-none bg-purple-700 border border-purple-500 focus:border-purple-300 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-purple-400 disabled:opacity-30" placeholder="Seu melhor e-mail…" aria-label="Seu melhor e-mail…" />
+                <button disabled={wasEmailSent || !email} type="submit" className="btn text-purple-600 bg-purple-100 hover:bg-white shadow disabled:opacity-30">Assinar</button>
               </div>
               {/* Success message */}
               {wasEmailSent &&
